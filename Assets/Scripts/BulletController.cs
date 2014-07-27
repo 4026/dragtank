e879f5140d2a32e3603b7dadadabe1a7b307eee4
Ben Hallett <name4026@gmail.com>
2014-07-27 21:48:25 +0100

    Added firing, recoil, improved turret target tracking

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+
+    public Vector3 target;
+    public float speed;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 to_target = target - transform.position;
+
+        if (to_target.magnitude < speed * Time.deltaTime)
+        {
+            transform.position = target;
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, target, (speed / to_target.magnitude) * Time.deltaTime);
+        }
+
+    }
+}
--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -3,23 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class TurretController : MonoBehaviour {
+public class TurretController : MonoBehaviour
+{
 
     public GameObject player;
     public float turnSpeed;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
 
     private List<GameObject> targets = new List<GameObject>();
+    private GameObject dragged_target;
     private bool animating = false;
 
-	void Update ()
+
+    void Update()
     {
 
         //Follow tank around
         transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
 
-        if (targets.Count > 0 && !animating)
+        if (!animating && (targets.Count > 0 || dragged_target != null))
         {
-            GameObject target = targets.First();
+            GameObject target = (targets.Count > 0) ? targets.First() : dragged_target;
 
             Vector3 target_from_self = target.transform.position - transform.position;
 
@@ -32,21 +37,24 @@ public class TurretController : MonoBehaviour {
             if (Mathf.Abs(bearing) < Time.deltaTime * turnSpeed)
             {
                 transform.rotation = lookRotation;
-                fireAt(target);
+                if (targets.Count > 0)
+                {
+                    fireAt(target);
+                }
             }
             else
             {
                 //rotate us over time according to speed until we are in the required rotation
                 transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * (turnSpeed / bearing));
             }
-
-            
-
-            
-
         }
 
-	}
+    }
+
+    public void setDraggedTarget(GameObject target)
+    {
+        dragged_target = target;
+    }
 
     public void addTarget(GameObject target)
     {
@@ -55,8 +63,28 @@ public class TurretController : MonoBehaviour {
 
     private void fireAt(GameObject target)
     {
-        animating = false;
+        //Spawn bullet
+        GameObject new_bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+        new_bullet.GetComponent<BulletController>().target = target.transform.position;
+        new_bullet.GetComponent<BulletController>().speed = bulletSpeed;
+
+        //Animate recoil
+        animating = true;
+        Vector3 recoil_pos = Vector3.down;
+        Hashtable itween_options = iTween.Hash(
+            "amount", recoil_pos,
+            "time", 0.5f,
+            "oncomplete", "animationEnd"
+        );
+        iTween.PunchPosition(this.gameObject, itween_options);
+
+        //Remove target indicator
         targets.Remove(target);
         GameObject.Destroy(target);
+    }
+
+    public void animationEnd()
+    {
+        animating = false;
     }
 }
