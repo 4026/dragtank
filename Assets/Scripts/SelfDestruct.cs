279855d23f6c43a4430721ef6023f6bc5f14e63f
Ben Hallett <name4026@gmail.com>
2016-03-18 12:57:56 +0000

    Replaced lame sprite explosions with awesome particle ones.

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float Lifetime;
+    private float m_startTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.time > m_startTime + Lifetime)
+        {
+            Destroy(gameObject);
+        }
+	
+	}
+}
